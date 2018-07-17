6313c477ba84bbd3dbed7b0e2c61d00084cc5481
RanyTheBard <30621232+RanyTheBard@users.noreply.github.com>
2018-07-17 14:52:18 +0800

    VTCRS

---
+++ swipe svg/CRS.js
@@ -0,0 +1,11 @@
+﻿$(".carousel").swipe({
+
+  swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+
+    if (direction == 'left') $(this).carousel('next');
+    if (direction == 'right') $(this).carousel('prev');
+
+  },
+  allowPageScroll:"vertical"
+
+});
